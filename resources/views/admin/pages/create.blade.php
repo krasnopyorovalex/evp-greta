9479895c0394b1def5b5043396c2fad57d06ccf8
Sanek <sanek@localhost.localdomain>
2021-06-15 20:43:52 +0300

    add gallery

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -17,7 +17,7 @@
             <form action="{{ route('admin.pages.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
 
-                @select(['name' => 'gallery_id', 'label' => 'Галерея', 'items' => $galleries])
+                @select(['name' => 'gallery_id', 'label' => 'Галерея', 'items' => $galleries, 'entity' => $page])
 
                 <div class="form-group">
                     <label for="template">Шаблон страницы:</label>
