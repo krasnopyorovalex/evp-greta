9e170c9d2e8c882ce699bdc5044f985e50c9221c
Sanek <sanek@localhost.localdomain>
2021-06-16 16:33:47 +0300

    fix date out

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -1,7 +1,7 @@
 <p>Этаж: {{ $data['excursion'] }}</p>
 <p>ФИО: {{ $data['fio'] }}</p>
 <p>Дата заезда: {{ $data['date_in'] }}</p>
-<p>Дата выезда: {{ $data['date_in'] }}</p>
+<p>Дата выезда: {{ $data['date_out'] }}</p>
 <p>Количество: {{ $data['count_adults'] }}</p>
 <p>Телефон: {{ $data['phone'] }}</p>
 @if($data['message'])
